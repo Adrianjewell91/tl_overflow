96bb6d8f655b7c27f5a608fed42d7fb73a47bc16
Adrian <adrianjewell91@gmail.com>
2017-10-08 20:45:28 -0700

    typo

--- documents/urls.py
+++ documents/urls.py
@@ -3,5 +3,5 @@ from documents import views
 
 urlpatterns = [
     url(r'^documents/$',views.document_list),
-    url(r'^documents/(?P<pk>[0-9]+)/$',views.document_list)
+    url(r'^documents/(?P<pk>[0-9]+)/$',views.document_detail)
 ]
