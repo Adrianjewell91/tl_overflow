21d91cf895ee3b47e586dd8aba378b41c6828389
Porfirio Matias <SSJ6Porfy@hotmail.com>
2017-10-09 13:11:57 -0700

    added rest_framework auth token in settings

--- backend/urls.py
+++ backend/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('documents.urls'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include('documents.urls')),
 ]
