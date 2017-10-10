659e8daa4726cf49b24f1c507a3fdc742cd08202
Porfirio Matias <SSJ6Porfy@hotmail.com>
2017-10-10 11:00:12 -0700

    removed slashes from requests and urls

--- documents/urls.py
+++ documents/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from documents import views
 
 urlpatterns = [
-    url(r'^documents/$',views.document_list),
+    url(r'^documents$',views.document_list),
     url(r'^documents/(?P<pk>[0-9]+)$',views.document_detail)
 ]
