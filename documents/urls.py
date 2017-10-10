56550c473c741ac5b548e6c9beba6bd6c4ac3666
Porfirio Matias <SSJ6Porfy@hotmail.com>
2017-10-10 11:53:58 -0700

    with adrian changes

--- documents/urls.py
+++ documents/urls.py
@@ -2,12 +2,6 @@ from django.conf.urls import url
 from documents import views
 from rest_framework import routers
 
-<<<<<<< HEAD
-urlpatterns = [
-    url(r'^documents$',views.document_list),
-    url(r'^documents/(?P<pk>[0-9]+)$',views.document_detail)
-]
-=======
 router = routers.SimpleRouter()
 router.register(r'documents', views.DocumentViewSet)
 router.register(r'translations', views.TranslationViewSet)
@@ -21,4 +15,3 @@ urlpatterns = router.urls
 #     url(r'^translations/$',views.translation_list),
 #     url(r'^translations/(?P<pk>[0-9]+)/$',views.translation_detail)
 # ]
->>>>>>> d52c7de7c93e47bf803619074f6996acf66c4e5f
