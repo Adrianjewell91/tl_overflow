eafa8ce9204e498e8361b705c4572af9f30cf333
Adrian <adrianjewell91@gmail.com>
2017-10-09 17:22:38 -0700

    made migration

--- documents/models.py
+++ documents/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-
 class Document(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     language = models.CharField(max_length=100, blank=True, default='')
     body = models.TextField()
+
+    class Meta:
+        ordering = ('created',)
+
+class Translation(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    language = models.CharField(max_length=100, blank=True, default='')
+    body = models.TextField()
+    original_doc_id = models.ForeignKey(Document)
 
     class Meta:
         ordering = ('created',)
