9228e6a7dc32075d28a64380f79fb4d0e05e772a
Porfirio Matias <SSJ6Porfy@hotmail.com>
2017-10-11 22:56:56 -0700

    passed first test for user auth

--- backend/urls.py
+++ backend/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include('documents.urls')),
+    url(r'^users/', include('accounts.urls')),
     url(r'^',views.ReactAppView.as_view()),
 ]
