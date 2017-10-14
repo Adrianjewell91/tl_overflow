133fe9541820c6c933afd9ee1e16aa11065ef12c
Adrian <adrianjewell91@gmail.com>
2017-10-13 22:12:11 -0700

    made link button

--- backend/urls.py
+++ backend/urls.py
@@ -12,9 +12,9 @@ import os
 
 urlpatterns = [
     url(r'^logout/$', auth_views.logout, name='logout'),
-    url(r'^login/$', auth_views.login, {'template_name': 'admin/login.html'}, name='login'),
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('documents.urls')),
     url(r'^users/', include('accounts.urls',namespace='accounts',app_name='accounts')),
     url(r'^overflow',views.ReactAppView.as_view()),
+    url(r'^', auth_views.login, {'template_name': 'admin/login.html'}, name='login'),
 ]
