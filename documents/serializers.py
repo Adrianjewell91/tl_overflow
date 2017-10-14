be06ce66c5d0db49e4fb4b897de45fbd8d8d5f38
Adrian <adrianjewell91@gmail.com>
2017-10-14 14:46:36 -0700

    pulled porfy

--- documents/serializers.py
+++ documents/serializers.py
@@ -25,8 +25,4 @@ class DocumentSerializer(serializers.ModelSerializer):
 class TranslationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Translation
-<<<<<<< HEAD
-        fields = ('id','title','body','language')
-=======
         fields = ('id','title','body','language', 'doc_id')
->>>>>>> db24869d1403f1cbc607720416c517e47891a449
