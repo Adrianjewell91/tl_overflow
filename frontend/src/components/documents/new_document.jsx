53e3ce86c236cabeb604cbbab568ede0d93f424e
Klhang <chrisjunhang@gmail.com>
2017-10-14 13:45:44 -0700

    finished redirect for new page

--- frontend/src/components/documents/new_document.jsx
+++ frontend/src/components/documents/new_document.jsx
@@ -37,6 +37,9 @@ class DocumentForm extends React.Component {
               body: `${res.data.text[0]}`,
               language: `${res.data.lang}`
             });
+          })
+          .then(() => {
+            this.props.history.push(`/documents/${resFromCreate.a_document.id}`);
           });
       })
       .then(() => {
@@ -54,6 +57,9 @@ class DocumentForm extends React.Component {
     const Langs = {
       Azerbaijan: "az",Malayalam: "ml",Albanian: "sq", Maltese: "mt", Amharic: "am", Macedonian: "mk", English: "en", Maori: "mi", Arabic: "ar", Marathi: "mr", Armenian: "hy", Mari: "mhr", Afrikaans: "af", Mongolian: "mn", Basque: "eu", German: "de", Bashkir: "ba", Nepali: "ne", Belarusian: "be", Norwegian: "no", Bengali: "bn", Punjabi: "pa", Burmese: "my", Papiamento: "pap", Bulgarian: "bg", Persian: "fa", Bosnian: "bs", Polish: "pl", Welsh: "cy", Portuguese: "pt", Hungarian: "hu", Romanian: "ro", Vietnamese: "vi", Russian: "ru", Haitian: "ht", Cebuano: "ceb", Galician: "gl", Serbian: "sr", Dutch: "nl", Sinhala: "si", Slovakian: "sk", Greek: "el", Slovenian: "sl", Georgian: "ka", Swahili: "sw", Gujarati: "gu", Sundanese: "su", Danish: "da", Tajik: "tg", Hebrew: "he", Thai: "th", Yiddish: "yi", Tagalog: "tl", Indonesian: "id", Tamil: "ta", Irish: "ga", Tatar: "tt", Italian: "it", Telugu: "te",Icelandic: "is",Turkish: "tr",Spanish: "es",Udmurt: "udm",Kazakh: "kk",Uzbek: "uz",Kannada: "kn",Ukrainian: "uk",Catalan: "ca",Urdu: "ur",Kyrgyz: "ky",Finnish: "fi",Chinese: "zh",French: "fr",Korean: "ko",Hindi: "hi",Xhosa: "xh",Croatian: "hr",Khmer: "km",Czech: "cs",Laotian: "lo",Swedish: "sv",Latin: "la",Scottish: "gd",Latvian: "lv",Estonian: "et",Lithuanian: "lt",Esperanto: "eo",Luxembourgish: "lb",Javanese: "jv",Malagasy: "mg",Japanese: "ja",Malay: "ms"
     };
+
+    const LangsKeys = Object.keys(Langs).sort()
+
     return (
       <div>
         <h1>New Document</h1>
@@ -67,7 +73,7 @@ class DocumentForm extends React.Component {
           <br />
 
           <textarea
-            rows="10"
+            rows="20"
             cols="100"
             type="text"
             placeholder="What do you want to translate?"
@@ -80,7 +86,7 @@ class DocumentForm extends React.Component {
             <option value="Select Language" disabled="true" selected="true">
               From
             </option>
-            {Object.keys(Langs).map(lang => (
+            {LangsKeys.map(lang => (
               <option value={Langs[lang]}>{lang}</option>
             ))}
           </select>
@@ -89,7 +95,7 @@ class DocumentForm extends React.Component {
             <option value="Select Language" disabled="true" selected="true">
               To
             </option>
-            {Object.keys(Langs).map(lang => (
+            {LangsKeys.map(lang => (
               <option value={Langs[lang]}>{lang}</option>
             ))}
           </select>
