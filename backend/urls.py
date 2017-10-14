e10d9a40bad01b6daf955379b95c2ff172c5f693
Adrian <adrianjewell91@gmail.com>
2017-10-13 21:18:24 -0700

    chenging auth scheme

--- backend/urls.py
+++ backend/urls.py
@@ -5,9 +5,15 @@ from rest_framework import routers
 
 from documents import views
 
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+import os
+
 urlpatterns = [
+    url(r'^login/$', auth_views.login, {'template_name': 'admin/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', admin.site.urls),
-    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include('documents.urls')),
     url(r'^users/', include('accounts.urls',namespace='accounts',app_name='accounts')),
     url(r'^overflow',views.ReactAppView.as_view()),
