ff1bf106544fc46bfb441fe3265c08f178207aaa
Adrian <adrianjewell91@gmail.com>
2017-10-14 18:21:18 -0700

    style indexes

--- frontend/src/components/documents/index_item.jsx
+++ frontend/src/components/documents/index_item.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import '../../stylesheets/index_item.css';
 
 export const IndexItem = ({doc}) => {
   return (
-    <li>
-       ({doc.language}):{doc.title}: <Link to={`/documents/${doc.id}`}>See Translations</Link>
+    <li className="index-item">
+       <span>{doc.language}</span>
+       <div>{doc.title}: {doc.body.slice(0,25)}...</div>
+       <Link to={`/documents/${doc.id}`}>See Translations</Link>
     </li>
   );
 }
