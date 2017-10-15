6cf85d85f22d659c2708d412271b779712a0a4f3
Adrian <adrianjewell91@gmail.com>
2017-10-14 18:46:47 -0700

    style the index item

--- frontend/src/components/documents/index_item.jsx
+++ frontend/src/components/documents/index_item.jsx
@@ -3,11 +3,20 @@ import {Link} from "react-router-dom";
 import '../../stylesheets/index_item.css';
 
 export const IndexItem = ({doc}) => {
+  const slice = doc.body ? doc.body.slice(0,25) : "";
+
   return (
     <li className="index-item">
-       <span>{doc.language}</span>
-       <div>{doc.title}: {doc.body.slice(0,25)}...</div>
-       <Link to={`/documents/${doc.id}`}>See Translations</Link>
+       <div className="index-item-stats">
+         <span className="language-tag">Language: {doc.language}</span>
+         <div className="index-title">Title: {doc.title}</div>
+         <div>Preview: "{slice}..."</div>
+       </div>
+
+       <div className="index-item-button-div">
+           <Link to={`/documents/${doc.id}`}>
+             <button className="show-button">See Translations</button></Link>
+       </div>
     </li>
   );
 }
